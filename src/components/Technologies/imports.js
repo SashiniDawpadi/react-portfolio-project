28cbb9e14fb2b718a91bb24a169c04007b25cc3e
SashiniDawpadi <sashinidawpadi@gmail.com>
2025-08-26 14:56:02 +0530

    renamed : src/components/Technologies/imports.js -> src/components/Technologies/imports.js (100%)

--- src/components/Technologies/imports.js
+++ src/components/Technologies/imports.js
@@ -1,14 +1,14 @@
 import netCore from '../../assets/images/netCore.png'
 import cSharp from '../../assets/images/cSharp.png'
 import spring from '../../assets/images/Spring.png'
-import selenium from '../../assets/images/selenium.png'
-import python from '../../assets/images/python.png'
+import selenium from '../../assets/images/Selenium.png'
+import python from '../../assets/images/Python.png'
 import java from '../../assets/images/Java.png'
-import mssql from '../../assets/images/mssql.png'
+import mssql from '../../assets/images/MSSQL.png'
 import mongo from '../../assets/images/mongo.png'
 import cLang from '../../assets/images/cLang.png'
 import fastAPI from '../../assets/images/fastAPI.png'
-import polygon from '../../assets/images/polygon.png'
+import polygon from '../../assets/images/Polygon.png'
 
 export {
   netCore,
